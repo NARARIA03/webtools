7e8d971e724b343358dfb01d5b566bcf8872e1f6
NARARIA03 <hyeonseong0305@gmail.com>
2024-08-10 05:17:01 +0900

    chore: Sitemap 생성 완료
    하나의 웹툴 만들때마다 /app/sitemap.ts 파일을 추가 / 수정해야 하는 단점
    은 있다...

---
+++ src/app/sitemap.ts
@@ -0,0 +1,138 @@
+import type { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: "https://webtoolstack.com",
+      lastModified: new Date(),
+      alternates: {
+        languages: {
+          ko: "https://webtoolstack.com/ko",
+          en: "https://webtoolstack.com/en",
+        },
+      },
+      changeFrequency: "weekly",
+      priority: 1,
+    },
+    {
+      url: "https://webtoolstack.com/developer",
+      lastModified: new Date(),
+      alternates: {
+        languages: {
+          ko: "https://webtoolstack.com/ko/developer",
+          en: "https://webtoolstack.com/en/developer",
+        },
+      },
+      changeFrequency: "weekly",
+      priority: 0.8,
+    },
+    {
+      url: "https://webtoolstack.com/text",
+      lastModified: new Date(),
+      alternates: {
+        languages: {
+          ko: "https://webtoolstack.com/ko/text",
+          en: "https://webtoolstack.com/en/text",
+        },
+      },
+      changeFrequency: "weekly",
+      priority: 0.8,
+    },
+    {
+      url: "https://webtoolstack.com/daily",
+      lastModified: new Date(),
+      alternates: {
+        languages: {
+          ko: "https://webtoolstack.com/ko/daily",
+          en: "https://webtoolstack.com/en/daily",
+        },
+      },
+      changeFrequency: "weekly",
+      priority: 0.8,
+    },
+    {
+      url: "https://webtoolstack.com/image",
+      lastModified: new Date(),
+      alternates: {
+        languages: {
+          ko: "https://webtoolstack.com/ko/image",
+          en: "https://webtoolstack.com/en/image",
+        },
+      },
+      changeFrequency: "weekly",
+      priority: 0.8,
+    },
+    {
+      url: "https://webtoolstack.com/file",
+      lastModified: new Date(),
+      alternates: {
+        languages: {
+          ko: "https://webtoolstack.com/ko/file",
+          en: "https://webtoolstack.com/en/file",
+        },
+      },
+      changeFrequency: "weekly",
+      priority: 0.8,
+    },
+    {
+      url: "https://webtoolstack.com/test",
+      lastModified: new Date(),
+      alternates: {
+        languages: {
+          ko: "https://webtoolstack.com/ko/test",
+          en: "https://webtoolstack.com/en/test",
+        },
+      },
+      changeFrequency: "weekly",
+      priority: 0.8,
+    },
+    {
+      url: "https://webtoolstack.com/math",
+      lastModified: new Date(),
+      alternates: {
+        languages: {
+          ko: "https://webtoolstack.com/ko/math",
+          en: "https://webtoolstack.com/en/math",
+        },
+      },
+      changeFrequency: "weekly",
+      priority: 0.8,
+    },
+    {
+      url: "https://webtoolstack.com/lottery",
+      lastModified: new Date(),
+      alternates: {
+        languages: {
+          ko: "https://webtoolstack.com/ko/lottery",
+          en: "https://webtoolstack.com/en/lottery",
+        },
+      },
+      changeFrequency: "weekly",
+      priority: 0.8,
+    },
+    {
+      url: "https://webtoolstack.com/developer/base64EncodeDecode",
+      lastModified: new Date(),
+      alternates: {
+        languages: {
+          ko: "https://webtoolstack.com/ko/developer/base64EncodeDecode",
+          en: "https://webtoolstack.com/en/developer/base64EncodeDecode",
+        },
+      },
+      changeFrequency: "yearly",
+      priority: 0.6,
+    },
+    {
+      url: "https://webtoolstack.com/text/countLetters",
+      lastModified: new Date(),
+      alternates: {
+        languages: {
+          ko: "https://webtoolstack.com/ko/text/countLetters",
+          en: "https://webtoolstack.com/en/text/countLetters",
+        },
+      },
+      changeFrequency: "yearly",
+      priority: 0.6,
+    },
+  ];
+}
