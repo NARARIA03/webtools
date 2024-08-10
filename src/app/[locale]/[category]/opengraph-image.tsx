b8486f0800946ba237c2baba6b240a5da5b5719e
NARARIA03 <hyeonseong0305@gmail.com>
2024-08-10 09:00:48 +0900

    chore: favicon과 OG, Twitter 이미지 추가
    링크를 SNS에 공유할때 필요한 title, description, image를 추가,
    favicon도 추가해줬다.

---
+++ src/app/[locale]/[category]/opengraph-image.tsx
@@ -0,0 +1,40 @@
+import { ImageResponse } from "next/og";
+
+export const runtime = "edge";
+
+// Image metadata
+export const alt = "Web Tool Stack";
+export const size = {
+  width: 1200,
+  height: 630,
+};
+
+export const contentType = "image/png";
+
+// Image generation
+export default async function Image({ params }: { params: { locale: string; category: string } }) {
+  return new ImageResponse(
+    (
+      // ImageResponse JSX element
+      <div
+        style={{
+          fontSize: 96,
+          background: "white",
+          width: "100%",
+          height: "100%",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <p>Web Tool Stack</p>
+        <p>{params.category} Tools</p>
+      </div>
+    ),
+    // ImageResponse options
+    {
+      ...size,
+    }
+  );
+}
