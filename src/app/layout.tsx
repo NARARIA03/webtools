23989223c08e941739f2489ee8ec80e08fba7804
NARARIA03 <hyeonseong0305@gmail.com>
2024-08-15 05:30:23 +0900

    chore: Naver 서치 어드바이저 소유확인 메타태그 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,11 @@ export const metadata: Metadata = {
   title: "WebTool Stack - Utility Hub",
   description:
     "Discover a comprehensive collection of web and PC utilities designed to enhance your productivity and streamline your tasks.",
+  verification: {
+    other: {
+      "naver-site-verification": [process.env.NEXT_PUBLIC_NAVERID || ""],
+    },
+  },
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
