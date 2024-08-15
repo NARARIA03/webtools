42e2708eb211024fa163c77894ff76505cb5b43b
NARARIA03 <hyeonseong0305@gmail.com>
2024-08-15 09:20:29 +0900

    fix: metadata에 og 태그 빠져있던것들 추가
    title, url, type, description, siteName 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,14 +3,20 @@ import { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "WebTool Stack - Utility Hub",
-  description:
-    "Discover a comprehensive collection of web and PC utilities designed to enhance your productivity and streamline your tasks.",
+  description: "Discover a range of web and PC tools to boost productivity and simplify tasks.",
   verification: {
     other: {
       "naver-site-verification": [process.env.NEXT_PUBLIC_NAVERID || ""],
     },
   },
   metadataBase: new URL("https://webtoolstack.com/"),
+  openGraph: {
+    type: "website",
+    url: "https://webtoolstack.com",
+    title: "Web Tool Stack",
+    description: "Discover a range of web and PC tools to boost productivity and simplify tasks.",
+    siteName: "Web Tool Stack",
+  },
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
